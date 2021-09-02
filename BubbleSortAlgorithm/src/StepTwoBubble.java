a286d8dad59255acf208a5056ca4c2045c4b8098
Theekshana Hariscandra <47950733+wasthTheekshana@users.noreply.github.com>
2021-09-02 21:49:26 +0530

    step two
    step two

--- BubbleSortAlgorithm/src/StepTwoBubble.java
+++ BubbleSortAlgorithm/src/StepTwoBubble.java
@@ -1,2 +1,27 @@
 public class StepTwoBubble {
+    void TwoBubbleSort(int array[]){
+
+        int size = array.length;
+
+        for (int i = 0; i < size -1; i++){
+            boolean swipped = false;
+            System.out.println("Firsr swiped" + swipped);
+            for (int j = 0; j < size - i -1; j++){
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    swipped = true;
+                    System.out.println(swipped);
+                }
+            }
+
+            if (!swipped){
+                break;
+            }
+        }
+
+
+    }
+
 }
--- BubbleSortAlgorithm/src/Test.java
+++ BubbleSortAlgorithm/src/Test.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         int arrs[] = {64, 34, 25, 12, 22, 11, 90};
-        StepOneBubble sp = new StepOneBubble();
-        sp.BubbleSortS(arrs);
+//        StepOneBubble sp = new StepOneBubble();
+        StepTwoBubble sp = new StepTwoBubble();
+        sp.TwoBubbleSort(arrs);
 
         System.out.println("Sorted Array ");
         System.out.println(Arrays.toString(arrs));
