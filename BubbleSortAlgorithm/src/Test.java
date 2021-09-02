ad7987a01c06dedae4cd89bd57414c75a6099e4d
Theekshana Hariscandra <47950733+wasthTheekshana@users.noreply.github.com>
2021-09-02 21:36:41 +0530

    bubble sort step1
    step one method

---
+++ BubbleSortAlgorithm/src/Test.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int arrs[] = {64, 34, 25, 12, 22, 11, 90};
+        StepOneBubble sp = new StepOneBubble();
+        sp.BubbleSortS(arrs);
+
+        System.out.println("Sorted Array ");
+        System.out.println(Arrays.toString(arrs));
+    }
+}
