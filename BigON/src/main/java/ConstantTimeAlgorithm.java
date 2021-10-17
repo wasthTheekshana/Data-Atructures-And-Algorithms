b4f0f5d59ccd9b02d0e70de58c0da398b30b6307
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2021-10-17 23:30:01 +0530

    done

---
+++ BigON/src/main/java/ConstantTimeAlgorithm.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class ConstantTimeAlgorithm {
+
+    /**
+     *
+     * O(1) - Calculate the sum from 1 to N via arithmetic series formula
+     */
+    public BigInteger sumOfArithmeticSeries_via_formula(long n) {
+        return BigInteger.valueOf(n).multiply(BigInteger.valueOf(n + 1)).divide(BigInteger.valueOf(2));
+    }
+
+}
